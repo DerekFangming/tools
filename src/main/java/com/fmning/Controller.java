f57ccb84541052c8eb81522193b0f6551d306436
synfm@126.com <synfm@126.com>
2020-02-01 19:10:38 -0600

    Initial commit

---
+++ src/main/java/com/fmning/Controller.java
@@ -0,0 +1,15 @@
+package com.fmning;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping("/test")
+	public ResponseEntity<String> test() {
+		return ResponseEntity.ok("haha1");
+	}
+
+}
---
+++ src/test/java/com/fmning/FmningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
