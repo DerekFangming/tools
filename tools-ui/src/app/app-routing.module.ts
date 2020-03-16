674287bcfc47dd22f85bfe7d4d04af6b9f19e6d4
synfm@126.com <synfm@126.com>
2020-03-15 19:41:25 -0500

    Save state before editor

--- tools-ui/src/app/app-routing.module.ts
+++ tools-ui/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClipboardComponent } from './clipboard/clipboard.component';
 
 
 const routes: Routes = [
-  //{ path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'clipboard', component: ClipboardComponent },
+  { path: '**', redirectTo: '/clipboard', pathMatch: 'full' }
 ];
 
 @NgModule({
