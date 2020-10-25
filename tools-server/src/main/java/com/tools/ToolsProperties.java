f98c89e559471da8c7e4fee4cb6659273845b737
synfm@126.com <synfm@126.com>
2020-10-25 10:55:32 -0500

    Re secure endpoints

--- tools-server/src/main/java/com/tools/ToolsProperties.java
+++ tools-server/src/main/java/com/tools/ToolsProperties.java
@@ -4,11 +4,12 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
-@ConfigurationProperties(prefix = "sp-slack-integration")
+@ConfigurationProperties(prefix = "tools")
 public class ToolsProperties {
     private String production;
 
     public boolean isProduction() {
+        System.out.println(production);
         return Boolean.parseBoolean(production);
     }
 }
