12f4a2bab9271f3217c2c3b98ed9f8dd18aec511
synfm@126.com <synfm@126.com>
2020-12-26 10:47:39 -0600

    Commands update

--- tools-server/src/main/java/com/tools/service/DiscordService.java
+++ tools-server/src/main/java/com/tools/service/DiscordService.java
@@ -21,8 +21,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import javax.swing.text.html.Option;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Optional;
 
@@ -45,11 +45,18 @@ public class DiscordService {
 
                     String[] command = content.split("\\s+");
 
-                    MessageChannel channel = message.getChannel().block();
+                    MessageChannel channel = message.getChannel().block(Duration.ofSeconds(3));
                     Member member = event.getMember().get();
 
                     if ("help".equalsIgnoreCase(command[1])) {
-                        channel.createMessage("绑定你的Origin ID: yf apex link <Origin Id>\n发送组队邀请: yf apex 2=1").block();
+                        channel.createEmbed(spec ->
+                            spec
+                                    .setTitle("妖风电竞 bot指令")
+                                    .setDescription("**绑定Origin ID：**\n```yf apex link ID```\n将指令里的`ID`替换成你的Origin ID即可。" +
+                                            "只需绑定一次，绑定之后，每次使用yf指令组队，都将查询你的战绩。可以多次运行这个指令来修改或者更新你的Discord帐号对应的Origin ID。\n\n" +
+                                            "**发送组队邀请：**\n```yf apex 你想说的```\n指令里`你想说的`可以随意输入，比如2=1。它将出现在组队邀请的标题中。" +
+                                            "如果使用这条指令的时候你在妖风电竞的某个语音频道中，组队邀请会自带你当前的语音频道链接，方便其他玩家点击进入。")
+                        ).block(Duration.ofSeconds(3));
                     } else if ("apex".equalsIgnoreCase(command[1]) && "link".equalsIgnoreCase(command[2])) {
                         DiscordUser discordUser = DiscordUser.builder()
                                 .id(member.getId().asLong())
@@ -60,10 +67,14 @@ public class DiscordService {
 
                         discordUserRepo.save(discordUser);
 
-                        channel.createMessage(discordUser.getName() + "已绑定Origin ID: " + discordUser.getApexId()).block();
-                        return;
+                        channel.createMessage("<@" + discordUser.getId() + "> 你已绑定Origin ID: **" + discordUser.getApexId() + "**").block(Duration.ofSeconds(3));
                     } else if ("apex".equalsIgnoreCase(command[1])) {
                         String extras = "加入频道";
+                        String kills = "";
+                        String rankName = "";
+                        String rankAvatar = "";
+                        String inviteUrl = "";
+
                         if (command.length >= 3) {
                             String[] extrasArray = Arrays.copyOfRange(command, 1, command.length);
                             extras = String.join(" ", extrasArray);
@@ -71,7 +82,7 @@ public class DiscordService {
 
                         Optional<DiscordUser> discordUserOpt = discordUserRepo.findById(member.getId().asLong());
                         if (!discordUserOpt.isPresent()) {
-                            channel.createMessage(member.getUsername() + "未绑定Origin ID. 运行yf help查看如何使用.").block();
+                            channel.createMessage("<@" + member.getId().asString() + "> 你未绑定Origin ID。运行yf help查看如何绑定。").block(Duration.ofSeconds(3));
                             return;
                         }
 
@@ -80,70 +91,56 @@ public class DiscordService {
                         httpGet.setHeader("TRN-Api-Key", "0721ec03-b743-40ff-97fa-0d04568f655a");
 
 
-//                    String originAvatar = null;
-                        String kills = null;
-                        String rankName = null;
-                        String rankAvatar = null;
-                        String inviteUrl = null;
 
-                        try {
-                            HttpResponse response = httpClient.execute(httpGet);
-                            int status = response.getStatusLine().getStatusCode();
-                            if (status == 404) {
-                                channel.createMessage("无法找到这个origin ID： " + discordUser.getApexId()).block();
-                                return;
+                        HttpResponse response = httpClient.execute(httpGet);
+                        int status = response.getStatusLine().getStatusCode();
+                        if (status == 404) {
+                            channel.createMessage("<@" + discordUser.getId() + "> 你绑定的Origin ID **" + discordUser.getApexId() +
+                                    "** 不存在，请重新绑定。").block(Duration.ofSeconds(3));
+                            return;
+                        }
+                        String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+                        JSONObject json = new JSONObject(responseBody);
+
+                        JSONArray segments = json.getJSONObject("data").getJSONArray("segments");
+                        for (int i = 0; i < segments.length(); i++) {
+                            JSONObject segment = segments.getJSONObject(i);
+                            if (segment.getString("type").equals("overview")) {
+                                JSONObject stats = segment.getJSONObject("stats");
+                                kills = stats.getJSONObject("kills").getString("displayValue");
+
+                                JSONObject rankScore = stats.getJSONObject("rankScore");
+                                rankName = rankScore.getJSONObject("metadata").getString("rankName");
+                                rankAvatar = rankScore.getJSONObject("metadata").getString("iconUrl");
+                                break;
                             }
-                            String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
-                            JSONObject json = new JSONObject(responseBody);
-
-//                        originAvatar = json.getJSONObject("data").getJSONObject("platformInfo").getString("avatarUrl");
-
-                            JSONArray segments = json.getJSONObject("data").getJSONArray("segments");
-                            for (int i = 0; i < segments.length(); i++) {
-                                JSONObject segment = segments.getJSONObject(i);
-                                if (segment.getString("type").equals("overview")) {
-                                    JSONObject stats = segment.getJSONObject("stats");
-                                    kills = stats.getJSONObject("kills").getString("displayValue");
-
-                                    JSONObject rankScore = stats.getJSONObject("rankScore");
-                                    rankName = rankScore.getJSONObject("metadata").getString("rankName");
-                                    rankAvatar = rankScore.getJSONObject("metadata").getString("iconUrl");
-                                    break;
-                                }
-                            }
-                        } catch (Exception e) {
-                            throw new IllegalStateException(e);
                         }
 
-                        VoiceState voiceState = member.getVoiceState().block();
+                        VoiceState voiceState = member.getVoiceState().block(Duration.ofSeconds(3));
                         if (voiceState != null) {
-                            VoiceChannel voiceChannel = voiceState.getChannel().block();
+                            VoiceChannel voiceChannel = voiceState.getChannel().block(Duration.ofSeconds(3));
                             if (voiceChannel != null) {
-                                ExtendedInvite invite = voiceChannel.createInvite(spec -> spec.setMaxUses(0)).block();
+                                ExtendedInvite invite = voiceChannel.createInvite(spec -> spec.setMaxUses(0)).block(Duration.ofSeconds(3));
                                 inviteUrl = "https://discord.gg/" + invite.getCode();
                             }
                         }
 
-                        try {
-                            String finalRankAvatar = rankAvatar == null ? "" : rankAvatar;
-                            String finalRankName = rankName == null ? "" : rankName;
-                            String finalKills = kills == null ? "" : kills;
-                            String finalExtras = extras == null ? "" : extras;
-                            String finalInviteUrl = inviteUrl == null ? "" : inviteUrl;
-                            channel.createEmbed(spec ->
-                                    spec.setAuthor(member.getUsername(),
-                                            member.getAvatarUrl(),
-                                            member.getAvatarUrl())
-                                            .setThumbnail(finalRankAvatar)
-                                            .setTitle(finalExtras)
-                                            .setDescription(finalInviteUrl)
-                                            .addField("Origin", discordUser.getApexId(), true)
-                                            .addField("排位", finalRankName, true)
-                                            .addField("击杀", finalKills, true)).block();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                        String finalRankAvatar = rankAvatar;
+                        String finalRankName = rankName;
+                        String finalKills = kills;
+                        String finalExtras = extras;
+                        String finalInviteUrl = inviteUrl;
+                        channel.createEmbed(spec ->
+                                spec.setAuthor(member.getUsername() + " 请求Apex组队", null, member.getAvatarUrl())
+                                        .setThumbnail(finalRankAvatar)
+                                        .setTitle(finalExtras)
+                                        .setDescription(finalInviteUrl)
+                                        .addField("Origin", discordUser.getApexId(), true)
+                                        .addField("段位", finalRankName, true)
+                                        .addField("击杀", finalKills, true)).block(Duration.ofSeconds(3));
 
+                    } else {
+                        channel.createMessage("<@" + member.getId().asString() + "> 无法识别指令 **" + content + "**。请运行yf help查看指令说明。").block(Duration.ofSeconds(3));
                     }
 
                 }
