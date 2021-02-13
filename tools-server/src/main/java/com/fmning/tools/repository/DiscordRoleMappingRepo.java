f97f2b7faa056137e5109625631cab42a3050731
synfm@126.com <synfm@126.com>
2021-02-13 13:11:34 -0600

    patch

--- tools-server/src/main/java/com/fmning/tools/repository/DiscordRoleMappingRepo.java
+++ tools-server/src/main/java/com/fmning/tools/repository/DiscordRoleMappingRepo.java
@@ -30,7 +30,7 @@ public interface DiscordRoleMappingRepo extends CrudRepository<DiscordRoleMappin
 
     @Modifying
     @Transactional
-    @Query(value="delete from DiscordRoleMapping d where d.created < ?1")
+    @Query(value="delete from DiscordRoleMapping d where d.created < ?1 and enabled = false")
     void deleteByCreated(Instant created);
 
     List<DiscordRoleMapping> findByOwnerId(String ownerId);
