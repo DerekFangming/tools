97c56d1df26f42a4c6d72799bfbd807320c391e9
synfm@126.com <synfm@126.com>
2021-03-18 21:28:28 -0500

    Welcome message

--- tools-server/src/main/java/com/fmning/tools/service/discord/MemberJoinedEventListener.java
+++ tools-server/src/main/java/com/fmning/tools/service/discord/MemberJoinedEventListener.java
@@ -6,10 +6,7 @@ import com.fmning.tools.repository.DiscordUserLogRepo;
 import com.fmning.tools.type.DiscordUserLogActionType;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -53,16 +50,24 @@ public class MemberJoinedEventListener extends BaseEventListener {
                                 .setFooter(g.getWelcomeFooter(), null)
                                 .build()).queue();
                     }
+                }
 
-                    // Role
-                    if (g.getWelcomeRoleId() != null) {
-                        Role role = guild.getRoleById(g.getWelcomeRoleId());
-                        if (role != null) {
-                            guild.addRoleToMember(member.getId(), role).queue();
-                        }
+                // Role
+                if (g.getWelcomeRoleId() != null) {
+                    Role role = guild.getRoleById(g.getWelcomeRoleId());
+                    if (role != null) {
+                        guild.addRoleToMember(member.getId(), role).queue();
                     }
                 }
             });
+
+
+            PrivateChannel privateChannel = event.getUser().openPrivateChannel().complete();
+            privateChannel.sendMessage("**哈咯，" + event.getMember().getEffectiveName() + "，欢迎加入妖风电竞！**\n\n我是妖风电" +
+                    "竞的专属机器人，独一无二的噢！我最擅长的就是帮你组队啦！在妖风电竞的任意文字频道发送`yf a`就好啦！组队邀请还可以加上你想说的话" +
+                    "，查询并显示你当前的战绩并且生成上车链接方便其他玩家点击上车噢。详情可以通过发送`yf help invite`查看！\n\n除此之外，我还会唱歌" +
+                    "，祝福生日，吹牛，创建并分享Tag，创建语音频道等等。快去妖风电竞的文字频道使用`yf`命令和我打个招呼吧~").queue();
+
         } catch (Exception e) {
             logError(event, discordGuildRepo, e);
         }
