3cd7961a4f35dd7ec01d59446ae1b53bd0055001
Fangming Ning <synfm@126.com>
2024-03-11 10:03:39 -0500

    Links detection in clipboard

--- tools-ui/src/app/clipboard/clipboard.component.ts
+++ tools-ui/src/app/clipboard/clipboard.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, TemplateRef, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Clipboard } from '../model/clipboard';
 import { Title } from '@angular/platform-browser';
 import { DOCUMENT } from '@angular/common';
+import { NgbModalRef, NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap'
 
 @Component({
   selector: 'app-clipboard',
@@ -12,9 +13,17 @@ import { DOCUMENT } from '@angular/common';
 })
 export class ClipboardComponent implements OnInit {
 
-  value = '';
+  value = ''
+  links = []
+  modalRef: NgbModalRef
+  @ViewChild('linksModal', { static: true}) linksModal: TemplateRef<any>
+  ngbModalOptions: NgbModalOptions = {
+    backdrop : 'static',
+    keyboard : false,
+    centered: true
+  }
 
-  constructor(private http: HttpClient, private title: Title, @Inject(DOCUMENT) private document: Document) {
+  constructor(private http: HttpClient, private title: Title, @Inject(DOCUMENT) private document: Document, private modalService: NgbModal) {
     this.title.setTitle("Clipboard");
     this.http.get<Clipboard>(environment.urlPrefix + 'api/clipboard').subscribe(cb => {
       this.value = cb.content;
@@ -40,18 +49,33 @@ export class ClipboardComponent implements OnInit {
     this.value = '';
   }
 
-  copyToClipboard(){
+  copyAllToClipboard() {
+    this.copyToClipboard(this.value)
+  }
+
+  copyToClipboard(value){
     const selBox = document.createElement('textarea');
     selBox.style.position = 'fixed';
     selBox.style.left = '0';
     selBox.style.top = '0';
     selBox.style.opacity = '0';
-    selBox.value = this.value;
+    selBox.value = value;
     document.body.appendChild(selBox);
     selBox.focus();
     selBox.select();
     document.execCommand('copy');
     document.body.removeChild(selBox);
+  }
+
+  showLinksModal() {
+    this.links = []
+    var re = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gmi
+    var m
+    while (m = re.exec(this.value)) {
+      this.links.push(m[1])
+    }
+
+    this.modalRef = this.modalService.open(this.linksModal, this.ngbModalOptions)
   }
 
 }
