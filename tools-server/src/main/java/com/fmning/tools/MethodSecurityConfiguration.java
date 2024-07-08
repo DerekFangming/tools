65a36277e1f3e7131aaf2dfe1db7d414bab9198a
Fangming Ning <synfm@126.com>
2024-07-08 09:28:07 -0500

    Add configuration annotation

--- tools-server/src/main/java/com/fmning/tools/MethodSecurityConfiguration.java
+++ tools-server/src/main/java/com/fmning/tools/MethodSecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.fmning.tools;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+@Configuration
 @EnableMethodSecurity
 @ConditionalOnProperty("tools.production")
 public class MethodSecurityConfiguration {
