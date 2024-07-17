a2419b664fde8f3c09d243657843ecaaba7a3c21
Fangming Ning <synfm@126.com>
2024-07-16 22:48:04 -0500

    Update SpendingController.java

--- tools-server/src/main/java/com/fmning/tools/controller/SpendingController.java
+++ tools-server/src/main/java/com/fmning/tools/controller/SpendingController.java
@@ -1,23 +1,16 @@
 package com.fmning.tools.controller;
 
-import com.fmning.tools.domain.Email;
 import com.fmning.tools.domain.SpendingAccount;
-import com.fmning.tools.dto.EmailDto;
 import com.fmning.tools.repository.SpendingAccountRepo;
 import com.fmning.tools.repository.SpendingTransactionRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
-
-import static com.fmning.tools.util.WebUtil.TOTAL_COUNT;
 
 @CommonsLog
 @RestController
@@ -28,7 +21,6 @@ public class SpendingController {
     private final SpendingAccountRepo accountRepo;
     private final SpendingTransactionRepo transactionRepo;
 
-//    @GetMapping
     @RequestMapping(value = "/accounts", method = RequestMethod.GET)
     @PreAuthorize("hasAnyAuthority('ADMIN', 'SPENDING')")
     public List<SpendingAccount> getAccounts() {
@@ -37,16 +29,13 @@ public class SpendingController {
                 .toList();
     }
 
-//    @PostMapping
     @RequestMapping(value = "/accounts", method = RequestMethod.POST)
     @PreAuthorize("hasAnyAuthority('ADMIN', 'SPENDING')")
     public SpendingAccount createAccount(@RequestBody SpendingAccount account) {
         account.setId(0);
-
         return accountRepo.save(account);
     }
 
-//    @PutMapping
     @RequestMapping(value = "/accounts/{id}", method = RequestMethod.PUT)
     @PreAuthorize("hasAnyAuthority('ADMIN', 'SPENDING')")
     public SpendingAccount updateAccount(@PathVariable int id, @RequestBody SpendingAccount account) {
@@ -59,7 +48,6 @@ public class SpendingController {
         return accountRepo.save(spendingAccount);
     }
 
-//    @DeleteMapping
     @RequestMapping(value = "/accounts/{id}", method = RequestMethod.DELETE)
     @PreAuthorize("hasAnyAuthority('ADMIN', 'SPENDING')")
     public void deleteAccount(@PathVariable int id) {
